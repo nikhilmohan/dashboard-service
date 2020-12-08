4b5ff532845d616bd33b43b5c472c87149312f2b
nikhilmohan <nikmohan81@gmail.com>
2020-12-08 12:17:36 +0530

    Updated for k8s deployment

--- src/main/java/com/nikhilm/hourglass/dashboard/services/DashboardService.java
+++ src/main/java/com/nikhilm/hourglass/dashboard/services/DashboardService.java
@@ -8,12 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cglib.core.Local;
-import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
-import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
-import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
