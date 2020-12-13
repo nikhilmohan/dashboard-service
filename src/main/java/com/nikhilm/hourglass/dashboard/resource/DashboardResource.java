2cf85aa106df59b521a360abb95fac0afc4129c0
nikhilmohan <nikmohan81@gmail.com>
2020-12-13 11:05:40 +0530

    API Doc updated

--- src/main/java/com/nikhilm/hourglass/dashboard/resource/DashboardResource.java
+++ src/main/java/com/nikhilm/hourglass/dashboard/resource/DashboardResource.java
@@ -1,9 +1,18 @@
 package com.nikhilm.hourglass.dashboard.resource;
 
+import com.nikhilm.hourglass.dashboard.exceptions.ApiError;
 import com.nikhilm.hourglass.dashboard.exceptions.DashboardException;
 import com.nikhilm.hourglass.dashboard.models.DashboardMetric;
 import com.nikhilm.hourglass.dashboard.models.MetricResponse;
 import com.nikhilm.hourglass.dashboard.services.DashboardService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
@@ -15,6 +24,14 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @Slf4j
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Dashboard service API",
+                version = "1.0",
+                description = "API for managing user statistics regarding goals and tasks",
+                contact = @Contact(name = "Nikhil Mohan", email = "nikmohan81@gmail.com")
+        )
+)
 public class DashboardResource {
 
     @Autowired
@@ -30,6 +47,14 @@ public class DashboardResource {
         rcb = factory.create("dashboard");
     }
 
+    @Operation(summary = "List all goal/ task metrics for the user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Metrics data received for user",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = MetricResponse.class)) }),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)) })})
     @GetMapping("/metrics")
     public Mono<MetricResponse> getMetrics(@RequestHeader("user") String user)  {
         log.info("Request received " + user);
@@ -37,6 +62,12 @@ public class DashboardResource {
                 throwable -> Mono.error(new DashboardException(500, "Internal server error!")));
     }
 
+    @Operation(summary = "Initialize metrics resource for user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Metrics resource initialized",
+                    content = { @Content }),
+            @ApiResponse(responseCode = "403", description = "Forbidden access",
+                    content = { @Content})})
     @PostMapping("/metrics/{userId}")
     public Mono<ResponseEntity<Object>> initMetrics(@PathVariable("userId") String userId,
                                                     @RequestHeader("user") String user)   {
